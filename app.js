4f200eefe59cc097ea983509bbcd8059ffe503ce
MadisonDWSNHU <madisonwierman@gmail.com>
2024-12-13 21:42:02 -0500

    Module7

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var handlebars = require("hbs");
+
+var passport = require("passport");
+require("./app_api/models/db");
+require("./app_api/config/passport");
 
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
@@ -15,13 +21,10 @@ var contactRouter = require("./app_server/routes/contact");
 var aboutRouter = require("./app_server/routes/about");
 var apiRouter = require("./app_api/routes/index");
 
-require("./app_api/models/db");
-
 var app = express();
 
-// view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
-// register handlebars partials (https://www.npmjs.com/package/hbs)
+
 handlebars.registerPartials(__dirname + "/app_server/views/partials");
 app.set("view engine", "hbs");
 
@@ -30,11 +33,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
@@ -55,11 +60,15 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+
   res.status(err.status || 500);
   res.render("error");
 });
-
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
+});
 module.exports = app;
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -3,16 +3,12 @@ const Trip = require("../models/travlr");
 const Model = mongoose.model("trips");
 
 const tripsList = async (req, res) => {
-  const q = await Model.find({}) // No filter, return all records
+  const q = await Model.find({})
     .exec();
 
-  // console.log(q);
-
   if (!q) {
-    // Database returned no data
     return res.status(404).json(err);
   } else {
-    // Return resulting trip list
     return res.status(200).json(q);
   }
 };
@@ -20,20 +16,13 @@ const tripsList = async (req, res) => {
 const tripsFindByCode = async (req, res) => {
   const q = await Model.find({ code: req.params.tripCode }).exec();
 
-  // console.log(q);
-
   if (!q) {
-    // Database returned no data
     return res.status(404).json(err);
   } else {
-    // Return resulting trip list
     return res.status(200).json(q);
   }
 };
 
-// PUT: /trips/:tripCode - Updates a Trip
-// Regardless of outcome, response must include HTML status code
-// and JSON message to the requesting client
 const tripsUpdateTrip = async (req, res) => {
   console.log(req.params);
   console.log(req.body);
@@ -53,10 +42,8 @@ const tripsUpdateTrip = async (req, res) => {
     ).exec();
 
     if (!q) {
-      // Database returned no data
       return res.status(404).json({ message: "Trip not found" });
     } else {
-      // Return resulting updated trip
       return res.status(200).json(q);
     }
 
@@ -83,7 +70,6 @@ const tripsAddTrip = async (req, res) => {
   const q = await newtrip.save();
 
   if (!q) {
-    //Database returned no data
     return res.status(400).json(err);
   } else {
     return res.status(201).json(q);
